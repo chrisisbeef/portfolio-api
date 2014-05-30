3f8e1b9ce9a03493910d28971df84364c019b8ba
Andreas Søvik <arsovik@gmail.com>
2014-05-30 17:21:10 +0200

    Return error codes on error. Added check for missing fields.

--- routes/contact.js
+++ routes/contact.js
@@ -7,15 +7,19 @@ module.exports = {
             var db = request.server.plugins['hapi-mongodb'].db;
             var contact = request.payload;
 
+            if (!contact.name || !contact.email || !contact.message) {
+                throw new Error("Missing data.");
+            }
+
             db.collection('posts').insert({name: contact.name, email: contact.email, message: contact.message}, {w:1}, function(err, result) {
                 if (!err) {
                     reply({"status": "success"});
                 } else {
-                    reply({"status": "error", "message": err});
+                    reply({"status": "error", "message": err}).code(500);
                 }
             });
         } catch(error) {
-            reply({"status": "error", "message": error});
+            reply({"status": "error", "message": error}).code(500);
         }
     }
 }
