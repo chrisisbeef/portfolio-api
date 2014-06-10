f4f5a233695ce9125b218b0bc547c548677e80c9
Andreas Søvik <arsovik@gmail.com>
2014-06-10 20:49:00 +0200

    We enable CORS in dev

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var config = require('./common').config();
 var Hapi = require('hapi');
 
-var server = Hapi.createServer(config.hapi.host, config.hapi.port);
+var server = Hapi.createServer(config.hapi.host, config.hapi.port, config.hapi.options);
 
 var mongodb = require('./mongodb');
 mongodb(server, config.mongodb);
